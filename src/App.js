a240c95e81bb499197e14652d9c9a2ff978b3300
CHO YESEUL <ys.cho@gmail.com>
2022-05-14 21:47:58 +0900

    create components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+
+import Main from "../src/component/Main";
+import Introduction from "../src/component/Introduction";
+import GettingStart from "../src/component/GettingStart";
+import HowToUse from "../src/component/HowToUse";
+import Contributors from "../src/component/Contributors";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
+      <Introduction />
+      <GettingStart />
+      <HowToUse />
+      <Contributors />
     </div>
   );
 }
