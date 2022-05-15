254df7f4c216171e044403991931070ca5b6f9fb
CHO YESEUL <ys.cho@gmail.com>
2022-05-15 20:31:56 +0900

    styled-component setting

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,5 +1,14 @@
 import * as React from "react";
 
+// import icon
+import darkmode_icon from "../imgs/darkmode_icon.png";
+
 export default function Main() {
-  return <div>test</div>;
+  return (
+    <div>
+      <div>
+        <img src={darkmode_icon} alt="icon" />
+      </div>
+    </div>
+  );
 }
