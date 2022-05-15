4539ec570a3f98cf7bdeb11e694309b4bebecb65
CHO YESEUL <ys.cho@gmail.com>
2022-05-15 21:24:31 +0900

    publishing main content and practice applying css

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled, { DefaultTheme, StyledComponent } from "styled-components";
 
 // import icon
 import darkmode_icon from "../imgs/darkmode_icon.png";
@@ -6,9 +7,41 @@ import darkmode_icon from "../imgs/darkmode_icon.png";
 export default function Main() {
   return (
     <div>
-      <div>
-        <img src={darkmode_icon} alt="icon" />
-      </div>
+      <ContentsWrap>
+        <ImgWarp>
+          <img src={darkmode_icon} alt="icon" />
+        </ImgWarp>
+        <ContentsTextWarp>
+          <ContentsTitle>@ysms/ts-darkmode</ContentsTitle>
+          <ContentsDesc>👀How about change your web to darkmode?</ContentsDesc>
+        </ContentsTextWarp>
+      </ContentsWrap>
     </div>
   );
 }
+const ImgWarp: StyledComponent<"div", DefaultTheme> = styled.div`
+  width: 10.7vw;
+  height: 10.7vw;
+  text-align: center;
+  margin: 0 auto;
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+const ContentsWrap: StyledComponent<"div", DefaultTheme> = styled.div`
+  text-align: center;
+  margin-top: 11.7vw;
+  padding-bottom: 4vw;
+`;
+const ContentsTitle: StyledComponent<"p", DefaultTheme> = styled.p`
+  margin-bottom: 1.19vw;
+  font-size: 2.7vw;
+  font-weight: bold;
+`;
+const ContentsDesc: StyledComponent<"p", DefaultTheme> = styled.p`
+  margin: 0;
+  font-size: 1.19vw;
+  color: #707070;
+`;
+const ContentsTextWarp: StyledComponent<"div", DefaultTheme> = styled.div``;
--- src/css/GlobalStyles.ts
+++ src/css/GlobalStyles.ts
@@ -6,7 +6,6 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    text-align: center;
   }
 
 `;
